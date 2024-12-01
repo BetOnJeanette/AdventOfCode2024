c834de8762e8f68093c9d59b65ef4d7c75ccfa91
BetOnJeanette <jeanettecrull99@gmail.com>
2024-12-01 14:15:55 -0500

    show performance

--- day1/main.ts
+++ day1/main.ts
@@ -3,7 +3,8 @@ import { solveProblem as solveFirstProb } from "./part1"
 import { solveProblem as solveSecondProb } from "./part2" 
 
 const file = fs.readFileSync(__dirname + "/input.txt", 'utf8');
+console.time("solveProbs");
 const part1Sol = solveFirstProb(file)
-console.log(part1Sol);
 const part2Sol = solveSecondProb(file)
-console.log(part2Sol);
+console.log(part1Sol, part2Sol);
+console.timeEnd("solveProbs");
